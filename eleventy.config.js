0ca86846acddad70b97d2fed1b1be78d0065ed04
mrmicaiah <mrmicaiah@gmail.com>
2026-01-30 02:36:14 -0600

    Set up 11ty project structure

---
+++ eleventy.config.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+  // Passthrough copy for static assets
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  // Watch for changes in these directories
+  eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.addWatchTarget("src/js/");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+      data: "_data"
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
